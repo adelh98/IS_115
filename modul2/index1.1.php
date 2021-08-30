1ce9cf75f53dc6b311d06d8235deb7d56eb7785b
Adel <adelh@uia.no>
2021-08-30 19:03:44 +0200

    Småendringer i Oppg.1

--- modul2/index1.1.php
+++ modul2/index1.1.php
@@ -18,24 +18,19 @@
 </style>
 <body>
 <form method="post" action="#">
-  <h3>Tast inn navn og etternavn:</h3>
-    <label for="Fornavn">Fornavn:</label><br>
-    <input type="text" id="Fornavn" name="Fornavn"><br>
+  <h3>Tast inn etternavn:</h3>
     <label for="Etternavn">Etternavn:</label><br>
     <input type="text" id="Etternavn" name="Etternavn"><br><br>
     <input type="submit" value="Legg til">
 </form>
     <?php
         //Lager dynamiske variabler. Disse blir tatt inn fra bruker og lagret i variabelen og senere brukt i logikken.
-        $Fornavn = $_POST['Fornavn'];
         $Etternavn = $_POST['Etternavn'];
 
         //Lager "logikken" for de ulike svarene:
-        $Fornavn = ucfirst(strtolower($Fornavn)); //Her sørger jeg for at første bokstav blir gjort om til stor bokstav, og resten blir små uavhengig av CAPS lock eller ikke.
-        $Etternavn = ucfirst(strtolower($Etternavn)); 
-        
-        $Fulltnavn = $Fornavn.$Etternavn; //Her legger jeg sammen Fornavn og Etternavn og gjør det om til 1 string som jeg senere bruker for å telle antall bokstaver
-        $Lengde = strlen($Fulltnavn); //Her lager jeg en variabel som teller antall bokstaver i det fullenavnet
+        $Etternavn = ucfirst(strtolower($Etternavn));  //Her sørger jeg for at første bokstav blir gjort om til stor bokstav, og resten blir små uavhengig av CAPS lock eller ikke.
+
+        $Lengde = strlen($Etternavn); //Her lager jeg en variabel som teller antall bokstaver i det fullenavnet
 
         //Printer ut resultatene i en setning ved hjelp av "Echo" i en oversiktlig tabell. Kunne ha gjort at det lagret seg ved legge det inn i en datastruktur
         //Men følte ikke det var nødvendig.
@@ -43,12 +38,10 @@
         echo 
             "<table>
                 <tr>
-                    <td><b>Navn</b></td>
                     <td><b>Etternavn</b></td>
                     <td><b>Lengde (Bokstaver)</b></td>
                 </tr>
                 <tr>
-                    <td>$Fornavn</td>
                     <td>$Etternavn</td>
                     <td>$Lengde</td>
                 </tr>
