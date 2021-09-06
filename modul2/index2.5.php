849a05ce8740cc151eb532626b9313438b6595cd
Adel <adelh@uia.no>
2021-09-06 13:18:22 +0200

    Fikset oppg.5

--- modul2/index2.5.php
+++ modul2/index2.5.php
@@ -9,20 +9,36 @@
   <meta name="description" content="Oppgave 4">
   <h1>Oppgave 5</h1>
 </head>
+
 <body>
+<form action="index2.5.php" method="get">
+  <input type="hidden" name="act" value="run">
+  <input type="submit" value="Generer passord">
+</form>
     <?php
-        //Her må jeg lage min egen funksjon:
-        function randomPassord($length = 8) {  //Setter passord lengden på 8
-            $symboler = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ01234567889!@&#%*()_-=+:;,.?"; //Alle tilgjengelige symboler som kan brukes i passordet
+    //Lager min egen funksjon som skal være passord generatoren.
+    function passordGenerator($lengde){
+            $passord = ''; //Tom string
+            $passordSets = ['1234567890', 'ABCDEFGHJKLMNPQRSTUVWXYZ', 'abcdefghjkmnpqrstuvwxyz']; //Lager set for tilgjengelige symboler
 
-            $passord = substr(str_shuffle($symboler), 0, $length);
-            
-            return $passord; //Generer et tilfeldig passord av de symboelen i variablene $symboler  
-        }
-        
-        $passord = randomPassord(); //Trenger ikke å oppgi lengde, i og med at det er fast satt fra før av.
+    //Henter en tilfeldig symboler fra array listen
+    foreach ($passordSets as $passordSet) { //Lager en foreach løkke som går gjennom hvert element i listen $passordSets
+        $passord .= $passordSet[array_rand(str_split($passordSet))]; //Bruker funksjonen "array_rand" som velger tilfedlige symboler fra listen
+        //Og jeg bruker funksjonen "str_split" som en string til en array.
+    }   
 
-        echo "Ditt nye passord er: $passord";
+    // Sjekker at lengden er riktig
+    while (strlen($passord) < $lengde) {
+        $randomSet = $passordSets[array_rand($passordSets)];
+        $passord .= $randomSet[array_rand(str_split($randomSet))];
+    }
+    echo "<br>\nDitt nye passord er: $passord";
+    }
+    //Kaller funksjonen med angitt lengde(8):
+    if (!empty($_GET['act'])) {
+        passordGenerator(8);
+    }
+    
         
     ?>
 </body>
