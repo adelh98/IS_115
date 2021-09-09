3cc6c31f2db2db4106032843fc5f2696efc3ba29
Adel <adelh@uia.no>
2021-09-09 23:02:53 +0200

    Lagt til 2 oppg fil, ingen kode

---
+++ modul3/index3.2.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Oppgave 1</title>
+  <meta name="description" content="Oppgave 1">
+  <h1>Oppgave 1</h1>
+</head>
+<body>
+<form method="post" action="#">
+  <h3>Tast inn Navn og Alder:</h3>
+    <label for="Navn">Navn:</label><br>
+    <input type="text" id="Navn" name="Navn"><br><br>
+    <label for="Alder">Alder:</label><br>
+    <input type="number" id="Alder" name="Alder"><br><br>
+    <input type="submit" value="Sjekk myndighet">
+</form>
+    <?php
+      //Kode here..
+
+    ?>
+</body>
+</html>
