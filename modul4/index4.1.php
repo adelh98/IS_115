0890bdc0957bdabd13d96703989bcba41b7ea575
Adel <adelh@uia.no>
2021-09-15 11:14:30 +0200

    First Commit, Oppg.1 Fullført

---
+++ modul4/index4.1.php
@@ -0,0 +1,33 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Oppgave 1</title>
+  <meta name="description" content="Oppgave 1">
+  <h1>Oppgave 1</h1>
+</head>
+<body>
+    <?php
+    //Definerer Arrayen
+    $arr = array();
+
+    //Legger bestemte verdier med forhåndsbestemt index::
+    $arr[0] = "Ole";
+    $arr[3] = "Brum";
+    $arr[5] = "Gikk";
+    $arr[7] = "Med";
+    $arr[8] = "En";
+    $arr[15] = "Saks";
+
+    //Lager en løkke
+    foreach ($arr as $key=>$string) {
+        print_r("På index: $key har vi $string<br>");
+    }
+    
+
+    ?>
+</body>
+</html>
