6e4c1d10c1f3d5791c7f1ce82b8311cb0d5f49ee
Adel <adelh@uia.no>
2021-09-29 13:23:49 +0200

    Fullført oppg4

--- modul4/index4.4.php
+++ modul4/index4.4.php
@@ -18,10 +18,37 @@
 
         //Setter opp en foreach lække som går gjennom hvert element i listen og printer ut med print_r
         //Bekreftelsen på endring av informasjon til bruker
-        echo "<h3>Listen:</h3>";
+        echo "<h3>Listen før element erstattning:</h3>";
         foreach ($arr as $key => $value) {
-            print_r("<b>Index: $key</b> = $value</b><br>");
+            print_r("<b>Index: $key</b> = $value<br>");
         }
+        echo "<h3>Ferdig!</h3>";
+
+        //Lager en variabel som skal bli brukt til å erstatte elementene i en eksisterende liste.
+        $erstattning = array("UiA", "UiO", "Bilbo", "Baggings", "Mordor", "Ferrari", "Julenissen", "Ole Brum", "Nassenøff", "Er skikkelig tøff");
+
+        echo "<h3>Listen etter element erstattning:</h3>";
+            $erstattetListe = array_replace($arr, $erstattning);
+            foreach ($erstattetListe as $key => $value) {
+                print_r("<b>Index: $key</b> = $value<br>"); 
+            }
+            echo "<h3>Ferdig!</h3>";
+        
+        //Her lager jeg logikk og variabler for å starte indexen høyere:
+        $nyIndexStart = 10;
+  
+        $erstattetListe = array_combine(range($nyIndexStart, 
+                count($erstattetListe) + ($nyIndexStart-1)),
+                array_values($erstattetListe));
+  
+        echo "<h3>Listen etter Re-Indexing:</h3>";
+        
+        foreach ($erstattetListe as $key => $value) {
+            print_r("<b>Index: $key</b> = $value<br>"); 
+        }
+        echo "<h3>Ferdig!</h3>";
+
+  
     ?>
 </body>
 </html>
