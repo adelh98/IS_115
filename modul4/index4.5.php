512aa86fae1c4ea9195e8ce4111cd3aa7cad82f1
Adel <adelh@uia.no>
2021-09-29 16:37:47 +0200

    First commit oppg5

---
+++ modul4/index4.5.php
@@ -0,0 +1,81 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Oppgave 5</title>
+  <meta name="description" content="Oppgave 5">
+  <h1>Oppgave 5</h1>
+</head>
+<body>
+    <?php 
+    //Lager listen med deltakere uten noe "value" fordi de ikke har begynt enda
+        $deltakere = array(
+            "Adel" => "",
+            "Oskar" => "",
+            "Dzenet" => "",
+            "Adrian" => "",
+            "Ronald" => "",
+            "Petter" => "",
+            "Hercules" => "",
+            "Odin" => "",
+            "Felix" => "",
+            "Rolf" => ""
+        );
+    //Printer listen med deltakere
+        echo "<h3>Deltakerene i konkuransen er:</h3>";
+        foreach ($deltakere as $key => $value) {
+            print_r("Navn: $key<br>"); 
+        }
+        echo "<br>";
+    
+    //Setter opp random funksjonen
+        $max = 50;
+        $min = 1;
+        rand($min, $max);
+    //Lager ny liste med samme deltakere som tar in rand() funksjonen for å gi et tilfeldig tall til hver av deltakerne
+        $rand = array(
+            "Adel" => rand($min, $max),
+            "Oskar" => rand($min, $max),
+            "Dzenet" => rand($min, $max),
+            "Adrian" => rand($min, $max),
+            "Ronald" => rand($min, $max),
+            "Petter" => rand($min, $max),
+            "Hercules" => rand($min, $max),
+            "Odin" => rand($min, $max),
+            "Felix" => rand($min, $max),
+            "Rolf" => rand($min, $max)
+        );
+        
+    //Den nye listen spleises med den gamle for å få oppdatert tall og sorteres
+        $oppdatertListe = array_replace($deltakere, $rand);
+        arsort($oppdatertListe);
+        
+    //Lager en foreach-løkke som printer navn med poengsum ved siden av.
+        foreach ($oppdatertListe as $key => $value){
+            echo "Navn: $key || Score: $value<br>";
+
+        }
+        echo "Size test<br>";
+        $size = count($oppdatertListe);
+        for($i = 0; $i <= $size; $i++) {
+            slettFraListe($oppdatertListe);
+
+            foreach ($oppdatertListe as $key => $value){
+                echo "Navn: $key || Score: $value<br>";
+            }
+            return $size;
+        }
+        
+        function slettFraListe($list) {
+            unset($list[array_search(min($list), $list)]);
+            return $list;
+        }
+
+ 
+
+    ?>
+</body>
+</html>
