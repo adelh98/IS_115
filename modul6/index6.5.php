bf28e716f6af82d47af0a6ffb690324cff14442e
Adel <adelh@uia.no>
2021-11-14 15:04:43 +0100

    Lagt til Dynamisk søk/m feilmelding

--- modul6/index6.5.php
+++ modul6/index6.5.php
@@ -9,6 +9,12 @@
   <h1>Oppgave 5</h1>
 </head>
 <body>
+<form method="post" action="#">
+  <h3>Hvilken interesse er det du ønsker å se etter?:</h3>
+    <label for="Interesse">Interesse:</label><br>
+    <input type="text" id="Interesse" name="Interesse"><br><br>
+    <input type="submit" name="Søk" value="Søk">
+</form>
     <?php
 
     //Include fil med passord:
@@ -21,74 +27,70 @@
     if (!$conn) {
         die("Tilkobling misslykket: " . mysqli_connect_error());
     }
-    echo "Tilkobling vellykket!"; 
+    echo "<br>Tilkobling vellykket!<br><br>"; 
+    
+    if(isset($_REQUEST['Søk'])) {
 
-    $sql = "SELECT * FROM medlemmer WHERE Interesser = 'Gaming' OR Interesser2 = 'Gaming' GROUP BY Interesser2, Interesser";
-
-    // Setter sammen spørringen til tilkoblingen
-    $stmt = $conn->prepare( $sql );
-
-    // Utfører spørring
-    $stmt->execute();
-
-    // Henter resultat
-    $resultat = $stmt->get_result();
-
-    ?>
-
-    <html>
-
-            <!-- Lager en tabell som viser medlemmene i databasen -->
-            <table border="1" cellpadding="5" align="center" style="text-align:center">
-            <tr>
-                <th>Fornavn</th>
-                <th>Etternavn</th>
-                <th>Epost</th>
-                <th>Mobilnummer</th>
-                <th>Adresse</th>
-                <th>Kjønn</th>
-                <th>Fødseldato</th>
-                <th>Interesser</th>
-                <th>Interesser2</th>
-                <th>Kursaktiviteter</th>
-                <th>Kontigentstatus</th>
-            </tr>
-
-    <?php 
-
-    // Henter en rad om gangen fra databasen (dvs. ett og ett medlem)
-    while( $row = $resultat->fetch_assoc() ) 
-        { // Opening while
-
-    ?>
-
-        <tr>
-            <td><?php echo $row['Fornavn']; ?></td>
-            <td><?php echo $row['Etternavn']; ?></td>
-            <td><?php echo $row['Epost']; ?></td>
-            <td><?php echo $row['Mobilnummer']; ?></td>
-            <td><?php echo $row['Adresse']; ?></td>
-            <td><?php echo $row['Kjønn']; ?></td>
-            <td><?php echo $row['Fødselsdato']; ?></td>
-            <td><?php echo $row['Interesser']; ?></td>
-            <td><?php echo $row['Interesser2']; ?></td>
-            <td><?php echo $row['Kursaktiviteter']; ?></td>
-            <td><?php echo $row['Kontigentstatus']; ?></td>
-
-        </tr>
-
-        <?php
-            } // Closing while
-
-            // Avslutter spørring 
-            $stmt->close();
+        $interesse = $_REQUEST['Interesse']; //Henter søke paramter fra formen
+        $sql = "SELECT Fornavn, Etternavn, Interesser, Interesser2 FROM medlemmer WHERE Interesser = '$interesse' OR Interesser2 = '$interesse'";
+        
+        // Setter sammen spørringen til tilkoblingen
+        $stmt = $conn->prepare( $sql );
+        
+        // Utfører spørring
+        $stmt->execute();
+        //Sjekker for om spørringen kommer med 0 rader tilbake.
+        if ($stmt->num_rows === 0) {
+            echo "Det finnes ingen medlemmer med $interesse som interesse.<br><br>";
+        } else {
+            //Kjør pååå videreee
+        }
+        // Henter resultat
+        $resultat = $stmt->get_result();
+        
         ?>
-        </table>
+
+<html>
+    
+    <!-- Lager en tabell som viser medlemmene i databasen -->
+    <table border="1" cellpadding="5" align="left" style="text-align:center">
+        <tr>
+            <th>Fornavn</th>
+            <th>Etternavn</th>
+            <th>Interesser</th>
+            <th>Interesser2</th>
+        </tr>
         
         <?php 
-            // Avslutter databasetilkobling
-            $conn->close();
+
+// Henter en rad om gangen fra databasen (dvs. ett og ett medlem)
+    while( $row = $resultat->fetch_assoc() ) 
+    { // Opening while
+        
         ?>
+    
+    <tr>
+        <td><?php echo $row['Fornavn']; ?></td>
+        <td><?php echo $row['Etternavn']; ?></td>
+        <td><?php echo $row['Interesser']; ?></td>
+        <td><?php echo $row['Interesser2']; ?></td>
+        
+    </tr>
+    
+    <?php
+                } // Closing while
+                
+                // Avslutter spørring 
+                $stmt->close();
+                ?>
+            </table>
+            
+            <?php 
+                // Avslutter databasetilkobling
+                $conn->close();
+
+        }
+            ?>
 
 </body>
 </html>
