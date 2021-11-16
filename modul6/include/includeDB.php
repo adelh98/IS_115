0a2f6eaf528c093910783f351bc81b7c774c6c5d
Adel <adelh@uia.no>
2021-11-16 23:18:46 +0100

    Oppdatert include/includeDB.php

--- modul6/include/includeDB.php
+++ modul6/include/includeDB.php
@@ -1,7 +1,16 @@
 <?php  
-$servernavn = "localhost";
-$brukernavn = "root";
-$passord = "12345678"; //Ja, jeg vet. Ikke verdens beste passord.
-$database = "test1";
+    $servernavn = "localhost";
+    $brukernavn = "root";
+    $passord = "12345678"; //Ja, jeg vet. Ikke verdens beste passord.
+    $database = "test1";
+
+   //Lager connection til DB:
+   $conn = mysqli_connect($servernavn, $brukernavn, $passord, $database);
+
+   //Sjekker connection:
+   if (!$conn) {
+       die("Tilkobling misslykket: " . mysqli_connect_error());
+   }
+   echo "Tilkobling vellykket!";
 
 ?>
