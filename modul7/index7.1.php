ec9b4cffaa7f845e7fcd39f891021afeb4af68bc
Adel <adelh@uia.no>
2021-11-16 23:19:48 +0100

    First Commit Modul 7 og Ferdig

---
+++ modul7/index7.1.php
@@ -0,0 +1,82 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <title>Oppgave 1</title>
+  <meta name="description" content="Oppgave 1">
+  <h1>Oppgave 1</h1>
+</head>
+<body>
+    <?php
+    if(isset($_REQUEST['logginn'])) {
+
+        // Include fil med passord:
+        require_once("include/includeDB.php");
+
+        // Lager variabler for henting av passord og brukernavn
+        $brukernavn = $_REQUEST['bnavn'];
+        $passord = $_REQUEST['pord'];
+        
+        // Lager spørring
+        $sql = "SELECT Fornavn, Etternavn, Passord
+                FROM medlemmer WHERE Fornavn = ?";
+        
+        // Initialiserer spørringen og forberederen den m/ error catch
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)){
+            echo "Tilfeldig feil";
+            exit(); 
+        }
+        
+        // Binder sammen $brukernavn variabel med spørringen
+        mysqli_stmt_bind_param($stmt, "s", $brukernavn);
+        
+        // Utfører spørring
+        mysqli_stmt_execute($stmt);
+
+        // Henter resultat fra spørringen
+        $medlem = mysqli_stmt_get_result($stmt);
+
+        // Henter ut resultatet i form av en array
+        $bruker = mysqli_fetch_assoc($medlem);       
+
+        // Lager ny variabel for the krypterte passordet i Databasen.
+        $hashedPassord = $bruker['Passord'];
+       
+        // Sjekker passord inputen, mot det krypterte passordet i databasen
+        $sjekkPass = password_verify($passord, $hashedPassord);
+        
+        // Lager en løkke for å sjekke om passordet fungerer, hvis ikke så printer den feilmelding.
+        if($sjekkPass === TRUE) {
+            //Passord matcher, setter session:
+            session_start();
+            $_SESSION['Etternavn'] = $bruker['Etternavn'];
+            $_SESSION['Fornavn'] = $bruker['Fornavn'];
+
+            //Videresender brukeren til innsiden av systemet
+            header("Location: passordbeskyttet.php");
+            exit();
+        } else if ($sjekkPass === FALSE) {
+            echo "Brukernavn og/eller passord er ikke riktig.";
+        }
+        mysqli_stmt_close($stmt);
+    }
+    ?>
+
+<form action="<?php echo  $_SERVER['PHP_SELF']; ?>" method="post">
+    <div class="container">
+        <label for="bnavn"><b>Brukernavn</b></label>
+        <input type="text" placeholder="Skriv inn brukernavn" name="bnavn" required>
+
+        <label for="pord"><b>Passord</b></label>
+        <input type="password" placeholder="Skriv inn passord" name="pord" required>
+
+        <input type="submit" name="logginn" value="Logg inn">
+    </div>
+</form>
+
+</body>
+</html>
---
+++ modul7/passordbeskyttet.php
@@ -0,0 +1,22 @@
+<?php
+    session_start();
+    if(isset($_SESSION["Fornavn"]) !== true) {
+        header("Location: index7.1.php");
+        exit();
+    } else{
+        echo "Dersom du kan lese dette, så er du logget inn<br><br>";
+    }
+
+    if (isset($_REQUEST['loggut'])) {
+        session_start();
+        session_unset();
+        session_destroy();
+        header("Location: index7.1.php");
+        exit();
+    }
+?>
+
+
+<form action ="<?php echo $_SERVER['PHP_SELF'];?>" method="request">
+<input type="submit" name="loggut" value="Logg Ut">
+</form>
