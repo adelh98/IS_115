7c0435cb150175f9f01c0083071e063ffa541985
Adel <adelh@uia.no>
2021-11-21 19:06:59 +0100

    Oppg4 jallamekk

--- modul8/index8.4.php
+++ modul8/index8.4.php
@@ -8,13 +8,39 @@
   <title>Oppgave 1</title>
   <meta name="description" content="Oppgave 1">
   <h1>Oppgave 1</h1>
-</head>
+
+  <h3>Trykk linken under for å laste ned PDF-filen:</h3>
+  <form action="<?php echo  $_SERVER['PHP_SELF']; ?>" method="post">
+  <input type="submit" name="lastned "value="Last ned">
+  </form>
+
 <body>
     <?php
-   
-   
 
-   
+      $filepath = 'filer/Test.pdf';
+         
+         //Check the file exists or not
+         if(file_exists($filepath)) {
+           
+           //Define header information
+           header('Content-Description: File Transfer');
+           header('Content-Type: application/pdf');
+           header("Cache-Control: no-cache, must-revalidate");
+           header("Expires: 0");
+           header('Content-Disposition: attachment; filename="'.basename($filepath).'"');
+           header('Content-Length: ' . filesize($filepath));
+           header('Pragma: public');
+           
+           //Clear system output buffer
+           flush();
+           
+           //Read the size of the file
+           readfile($filepath);
+           
+           //Terminate from the script
+           die();
+          }
+          
     ?>
 
 </body>
