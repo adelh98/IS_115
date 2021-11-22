650372538dedf02ea4fe9a0590f53d45ebd3e544
Adel <adelh@uia.no>
2021-11-22 15:52:35 +0100

    Fikset oppg2. Fullført

--- modul8/index8.2.php
+++ modul8/index8.2.php
@@ -5,13 +5,33 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Oppgave 1</title>
+  <title>Oppgave 2</title>
   <meta name="description" content="Oppgave 1">
   <h1>Oppgave 1</h1>
 </head>
 <body>
     <?php
    
+   function wh_log($log_msg)
+   {
+       $log_filename = "log";
+       if (!file_exists($log_filename)) 
+       {
+           // create directory/folder uploads.
+           mkdir($log_filename, 0777, true);
+       }
+       $log_file_data = $log_filename.'/log_' . date('d-M-Y') . '.log';
+       // if you don't add `FILE_APPEND`, the file will be erased each time you add a log
+       file_put_contents($log_file_data, $log_msg . "\n", FILE_APPEND);
+   } 
+   
+   // call to function
+   wh_log("Nissetryne");
+
+      $data = file("C:/xampp/htdocs/IS_115/modul8/log/log_22-Nov-2021.log",FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES); //read the entire file to array by ignoring new lines and spaces
+        echo "<pre/>"; // print output of the above line
+      
+      print_r(array_slice($data, -10));
 
 
    
